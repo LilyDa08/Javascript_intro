1a6a11910647ad011b7848cc4ca33692621e0c69
Lily <lilydanhaive@gmail.com>
2018-10-24 11:08:30 +0200

    last push before erase

--- CookieClicker/js/main.js
+++ CookieClicker/js/main.js
@@ -1,9 +1,13 @@
 let clicDoc = document.querySelector("#clic");
+let priceZone = document.querySelector(".price");
+let cookieZone = document.querySelector(".cookie_zone");
 let affichageDoc = document.querySelector("#affichage");
 let multiZoneDoc = document.querySelector("#multiZone");
-let multipleDoc = document.querySelector('#multiplicateur');
+let multiPriceDoc = document.querySelector("#multiPrice");
+let multipleDoc = document.querySelector('#multiplicateur')
+let flagMulti = true;
 
-let minimum = 50;
+let minimum = 20;
 
 let score = 1;
 let multiButton = 1; // affiché sur le boutton
@@ -11,36 +15,46 @@ let multiScore = 1; // calculé pr le score
 let totalScore = 0;
 
 function startGame() {
-
     totalScore += score * multiScore;
 
-    if (totalScore%minimum === 0) {
-        multiButton++;
-        console.log(minimum);
-        multiZoneDoc.innerHTML = multiButton;
-        addCookie(); 
+        if (totalScore % minimum === 0) {
+            multiButton++;
+            multiZoneDoc.innerHTML = multiButton;
+            multiPriceDoc.innerHTML = multiButton * 10;
+            console.log("1 : " + multiButton);
+            addCookie();
+     
     }
     // do score + the extra score
     affichageDoc.innerHTML = totalScore;
 }
 
 function addCookie() {
-
+    priceZone.style.visibility = "visible";
     multipleDoc.style.visibility = "visible";
-    multipleDoc.addEventListener('click', augmenterMultiple);
-
+    console.log("2 : " + multiButton);
+    multipleDoc.addEventListener('click', increaseMultipler);
 }
 
-function augmenterMultiple() {
-    totalScore -= 50;
-    multiScore = multiButton;
-   // minimum = Math.pow(minimum, multiScore);
-    refreshVisuals();
-
-}
-
-function refreshVisuals() {
+function increaseMultipler() {
+    if (totalScore >= (multiButton * 10)) {
+        totalScore -= (multiButton * 10);
+        console.log("3 : " + multiButton);
+        multiScore = multiButton;
+        if (totalScore < (multiButton * 10)) {
+            priceZone.style.visibility = "hidden";
+            multipleDoc.style.visibility = "hidden";
+            console.log("4 : " + multiButton);
+        }
+    }
     affichageDoc.innerHTML = totalScore;
+    console.log("5 : " + multiButton);
+    return multiScore;
 }
+
+//function refreshVisuals() {
+//
+//    affichageDoc.innerHTML = totalScore;
+//}
 
 clicDoc.addEventListener("click", startGame);
